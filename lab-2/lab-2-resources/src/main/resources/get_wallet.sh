271fe83fcf37295b2c8b86e8acdfe6a0929c59fd
max.jahn@gmail.com <maximillian.jahn@oracle.com>
2019-07-23 08:37:38 +0200

    updated unzip path

--- lab-2/lab-2-resources/src/main/resources/get_wallet.sh
+++ lab-2/lab-2-resources/src/main/resources/get_wallet.sh
@@ -3,10 +3,10 @@
 if [ $1 ]
 then
         oci db autonomous-database generate-wallet --autonomous-database-id $1 --file src/main/resources/wallet.zip --password Oracle#1ab
-        unzip src/main/resources/wallet.zip
-        echo "Oracle#1ab" > src/main/resources/atp_password.txt
-
-
+        cd src/main/resources
+        unzip wallet.zip
+        echo "Oracle#1ab" > atp_password.txt
+        cd -
 
 else
         echo "\nusage: $0 ocid\n"
