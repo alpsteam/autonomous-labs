beac6033ec1e91b20cdc288ed59d7b6871a716d8
max.jahn@gmail.com <maximillian.jahn@oracle.com>
2019-07-23 08:06:06 +0200

    Added unzip

--- lab-2/lab-2-resources/src/main/resources/get_wallet.sh
+++ lab-2/lab-2-resources/src/main/resources/get_wallet.sh
@@ -2,7 +2,12 @@
 
 if [ $1 ]
 then
-        oci db autonomous-database generate-wallet --autonomous-database-id $1 --file wallet.zip --password Oracle#1
+        oci db autonomous-database generate-wallet --autonomous-database-id $1 --file src/main/resources/wallet.zip --password Oracle#1ab
+        unzip src/main/resources/wallet.zip
+        echo "Oracle#1ab" > src/main/resources/atp_password.txt
+
+
+
 else
         echo "\nusage: $0 ocid\n"
 fi
